0809b3cbd619df9c52eb2307e896d1f8141892b5
rliu6915 <87982475+rliu6915@users.noreply.github.com>
2022-08-09 13:15:27 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import Roles.Admin;
+import Roles.User;
+import View.Boundary;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+         new Boundary();
+
+    }
+}
